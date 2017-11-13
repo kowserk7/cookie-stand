e3bcdbfe9e8f435f5b7fc3defd0096a2a97acbb6
Kowser Kassa <kowserk7@gmail.com>
2017-11-13 13:48:57 -0800

    added linter and updated javascript

---
+++ app.js
@@ -0,0 +1,25 @@
+var Pike {
+  min: 23,
+  max: 65,
+  avg: 6.3,
+};
+var SeaTac {
+  min: 3,
+  max: 24,
+  avg: 1.2,
+};
+var SeattleCenter {
+  min: 11,
+  max: 38,
+  avg: 3.7,
+};
+var CapitolHill {
+  min: 20,
+  max: 38,
+  avg: 2.3,
+};
+var Alki {
+  min: 2,
+  max: 16,
+  avg: 4.6,
+};
