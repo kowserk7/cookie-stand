376e99123457f7da9dff6a5a224f6191a5a09a8d
Kowser Kassa <kowserk7@gmail.com>
2017-11-13 15:12:47 -0800

    Update

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-var Pike {
+var Pike = {
   min: 23,
   max: 65,
   avg: 6.3,
+  getRandom: function() {
+    return Math.random() * (this.max - this.min) + this.min;
+  },
+  hours : ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM','5 PM', '6 PM', '7 PM', '8 PM'],
+  avgCookieHr: function() {
+    for ( var i = 0; i < this.hours.length; i++){
+      console.log(this.hours[i]);
+    }
+  }
 };
-var SeaTac {
+var SeaTac = {
   min: 3,
   max: 24,
   avg: 1.2,
+  getRandom: function() {
+    return Math.random() * (this.max - this.min) + this.min;
+  },
 };
-var SeattleCenter {
+var SeattleCenter = {
   min: 11,
   max: 38,
   avg: 3.7,
+  getRandom: function() {
+    return Math.random() * (this.max - this.min) + this.min;
+  },
 };
-var CapitolHill {
+var CapitolHill = {
   min: 20,
   max: 38,
   avg: 2.3,
+  getRandom: function() {
+    var customer =  Math.random() * (this.max - this.min) + this.min;
+    console.log(customer);
+  },
 };
-var Alki {
+var Alki = {
   min: 2,
   max: 16,
   avg: 4.6,
+  getRandom: function() {
+    return Math.random() * (this.max - this.min) + this.min;
+  },
 };
