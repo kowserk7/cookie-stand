96b14fa090b73ff23277b09a6ed92c019c09adbd
Kowser Kassa <kowserk7@gmail.com>
2017-11-15 22:09:02 -0800

    last minute touches

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function Store (id, min, max, avg){
 }
 console.log(Store);
 
-new Store ('1st and Pike', 23, 65, 6.3);
+new Store ('1st & Pike', 23, 65, 6.3);
 new Store ('SeaTac Airport', 3, 24, 1.2);
 new Store ('Seattle Center', 11, 38, 3.7);
 new Store ('Capitol Hill', 20, 38, 2.3);
